b4d272317291c5e3f7958144580eb7087063bda3
academm <aasleptsov@gmail.com>
2015-05-14 12:04:15 +0300

    empowering panel StudentsFrame

--- src/main/java/pw/academ/students/frame/StudentsFrame.java
+++ src/main/java/pw/academ/students/frame/StudentsFrame.java
@@ -1,15 +1,23 @@
 package pw.academ.students.frame;
 
+import java.sql.SQLException;
 import java.util.Vector;
 
+import java.awt.Dimension;
 import java.awt.FlowLayout;
 import java.awt.BorderLayout;
+import java.awt.GridLayout;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JList;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
+import javax.swing.JTable;
 
 import javax.swing.JSpinner;
 import javax.swing.SpinnerModel;
@@ -17,22 +25,44 @@ import javax.swing.SpinnerNumberModel;
 import javax.swing.SwingUtilities;
 import javax.swing.border.BevelBorder;
 
-import pw.academ.students.logic.Group;
 import pw.academ.students.logic.ManagementSystem;
-import pw.academ.students.logic.Student;
+import pw.academ.students.logic.Group;
 
 public class StudentsFrame extends JFrame {
+    // Введем сразу имена для кнопок - потом будем их использовать в обработчиках
 
-    ManagementSystem ms = null;
+    private static final String MOVE_GR = "moveGroup";
+    private static final String CLEAR_GR = "clearGroup";
+    private static final String INSERT_ST = "insertStudent";
+    private static final String UPDATE_ST = "updateStudent";
+    private static final String DELETE_ST = "deleteStudent";
+    private static final String ALL_STUDENTS = "allStudent";
+
+    private ManagementSystem ms = null;
     private JList grpList;
-    private JList stdList;
+    private JTable stdList;
     private JSpinner spYear;
 
     public StudentsFrame() throws Exception {
+
         // Устанавливаем layout для всей клиентской части формы
         getContentPane().setLayout(new BorderLayout());
 
-        // Создаем верхнюю панел, где будет поле для ввода года
+        // Создаем строку меню
+        JMenuBar menuBar = new JMenuBar();
+        // Создаем выпадающее меню
+        JMenu menu = new JMenu("Отчеты");
+        // Создаем пункт в выпадающем меню
+        JMenuItem menuItem = new JMenuItem("Все студенты");
+        menuItem.setName(ALL_STUDENTS);
+        // Вставляем пункт меню в выпадающее меню
+        menu.add(menuItem);
+        // Вставляем выпадающее меню в строку меню
+        menuBar.add(menu);
+        // Устанавливаем меню для формы
+        setJMenuBar(menuBar);
+
+        // Создаем верхнюю панель, где будет поле для ввода года
         JPanel top = new JPanel();
         // Устанавливаем для нее layout
         top.setLayout(new FlowLayout(FlowLayout.LEFT));
@@ -40,8 +70,8 @@ public class StudentsFrame extends JFrame {
         // Вставляем пояснительную надпись
         top.add(new JLabel("Год обучения:"));
         // Делаем спин-поле
-        //   1. Задаем модель поведения - только цифры
-        //   2. Вставляем в панель
+        // 1. Задаем модель поведения - только цифры
+        // 2. Вставляем в панель
         SpinnerModel sm = new SpinnerNumberModel(2006, 1900, 2100, 1);
         spYear = new JSpinner(sm);
         top.add(spYear);
@@ -51,20 +81,16 @@ public class StudentsFrame extends JFrame {
         bot.setLayout(new BorderLayout());
 
         // Создаем левую панель для вывода списка групп
-        JPanel left = new JPanel();
+        // Она у нас
+        GroupPanel left = new GroupPanel();
         // Задаем layout и задаем "бордюр" вокруг панели
         left.setLayout(new BorderLayout());
-        left.setBorder(new BevelBorder(BevelBorder.RAISED));
+        left.setBorder(new BevelBorder(BevelBorder.LOWERED));
 
-        // Нам необходимо обработать ошибку при обращении к базе данных
-        Vector gr = null;
-        Vector st = null;
-        // Попробуем получить коннект к базе данных
+        // Получаем коннект к базе и создаем объект ManagementSystem
         ms = ManagementSystem.getInstance();
         // Получаем список групп
-        gr = new Vector<Group>(ms.getGroups());
-        // Получаем список студентов
-        st = new Vector<Student>(ms.getAllStudents());
+        Vector<Group> gr = new Vector<Group>(ms.getGroups());
         // Создаем надпись
         left.add(new JLabel("Группы:"), BorderLayout.NORTH);
         // Создаем визуальный список и вставляем его в скроллируемую
@@ -72,18 +98,47 @@ public class StudentsFrame extends JFrame {
         grpList = new JList(gr);
         left.add(new JScrollPane(grpList), BorderLayout.CENTER);
 
+        // Создаем кнопки для групп
+        JButton btnMvGr = new JButton("Переместить");
+        btnMvGr.setName(MOVE_GR);
+        JButton btnClGr = new JButton("Очистить");
+        btnClGr.setName(CLEAR_GR);
+        // Создаем панель, на которую положим наши кнопки и кладем ее вниз
+        JPanel pnlBtnGr = new JPanel();
+        pnlBtnGr.setLayout(new GridLayout(1, 2));
+        pnlBtnGr.add(btnMvGr);
+        pnlBtnGr.add(btnClGr);
+        left.add(pnlBtnGr, BorderLayout.SOUTH);
+
         // Создаем правую панель для вывода списка студентов
         JPanel right = new JPanel();
         // Задаем layout и задаем "бордюр" вокруг панели
         right.setLayout(new BorderLayout());
-        right.setBorder(new BevelBorder(BevelBorder.RAISED));
+        right.setBorder(new BevelBorder(BevelBorder.LOWERED));
 
         // Создаем надпись
         right.add(new JLabel("Студенты:"), BorderLayout.NORTH);
-        // Создаем визуальный список и вставляем его в скроллируемую
+        // Создаем таблицу и вставляем ее в скроллируемую
         // панель, которую в свою очередь уже кладем на панель right
-        stdList = new JList(st);
+        // Наша таблица пока ничего не умеет - просто положим ее как заготовку
+        // Сделаем в ней 4 колонки - Фамилия, Имя, Отчество, Дата рождения
+        stdList = new JTable(1, 4);
         right.add(new JScrollPane(stdList), BorderLayout.CENTER);
+        // Создаем кнопки для студентов
+        JButton btnAddSt = new JButton("Добавить");
+        btnAddSt.setName(INSERT_ST);
+        JButton btnUpdSt = new JButton("Исправить");
+        btnUpdSt.setName(UPDATE_ST);
+        JButton btnDelSt = new JButton("Удалить");
+        btnDelSt.setName(DELETE_ST);
+
+        // Создаем панель, на которую положим наши кнопки и кладем ее вниз
+        JPanel pnlBtnSt = new JPanel();
+        pnlBtnSt.setLayout(new GridLayout(1, 3));
+        pnlBtnSt.add(btnAddSt);
+        pnlBtnSt.add(btnUpdSt);
+        pnlBtnSt.add(btnDelSt);
+        right.add(pnlBtnSt, BorderLayout.SOUTH);
 
         // Вставляем панели со списками групп и студентов в нижнюю панель
         bot.add(left, BorderLayout.WEST);
@@ -93,8 +148,11 @@ public class StudentsFrame extends JFrame {
         getContentPane().add(top, BorderLayout.NORTH);
         getContentPane().add(bot, BorderLayout.CENTER);
 
+        // Сразу выделяем первую группу
+        grpList.setSelectedIndex(0);
+
         // Задаем границы формы
-        setBounds(100, 100, 600, 400);
+        setBounds(100, 100, 700, 500);
     }
 
     public static void main(String args[]) {
@@ -111,5 +169,13 @@ public class StudentsFrame extends JFrame {
                 }
             }
         });
+    }
+}
+
+// Наш внутренний класс - переопределенная панель.
+class GroupPanel extends JPanel {
+
+    public Dimension getPreferredSize() {
+        return new Dimension(250, 0);
     }
 }
